24b25123890010dd7766a5d0a37dc78a8537201c
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-18 16:32:17 -0700

    learning about modules

--- good_dog.rb
+++ good_dog.rb
@@ -1,7 +1,23 @@
+module Speak
+  def speak(sound)
+    puts sound
+  end
+end
+
 class GoodDog
+  include Speak
+end
+
+class HumanBeing
+  include Speak
 end
 
 sparky = GoodDog.new
+sparky.speak("Arf!")  # => Arf
+bob = HumanBeing.new
+bob.speak("Hello!")   # => Hello!
+
+
 
 
 # Here, we have created an instance of our GoodDog class and stored it in the
