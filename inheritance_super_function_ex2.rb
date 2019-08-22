cd0aed605fcd36a19e8a2db17357da8248cc1372
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-21 19:14:44 -0700

    learning about modules

---
+++ inheritance_super_function_ex2.rb
@@ -0,0 +1,25 @@
+class Animal
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class GoodDog < Animal
+  def initialize(color)
+    super
+    @color = color
+  end
+end
+
+class BadDog < Animal
+  def initialize(age,name)
+    super(name)
+    @age = age
+  end
+end
+
+BadDog.new(2, "bear")
+
+bruno = GoodDog.new("brown")
