034f09cda972c0fbc0adb19237e470a0641899aa
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-22 21:29:08 -0700

    class variables

--- book_exercises/inheritance_ex1.rb
+++ book_exercises/inheritance_ex1.rb
@@ -1,4 +1,14 @@
 class Vehicle
+  @@number_of_vehicles = 0
+
+  def self.number_of_vehicles
+    puts "This program has created #{@@number_of_vehicles}"
+  end
+
+  def initialize
+    @@number_of_vehicles = @@number_of_vehicles + 1
+  end
+
   def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas."
   end
@@ -9,5 +19,5 @@ class MyCar < Vehicle
 end
 
 class MyTruck < Vehicle
-  NUMBER_OF_DOORS = 2 
+  NUMBER_OF_DOORS = 2
 end
