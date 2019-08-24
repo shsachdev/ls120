396c965e45711af547fd3fe1c4213e0d3511e2fc
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-24 12:36:12 -0700

    self inside instance method and inside class

--- book_chapter_q2/good_dog_v10.rb
+++ book_chapter_q2/good_dog_v10.rb
@@ -8,7 +8,7 @@ class GoodDog
   end
 
   def change_info(n, h, w) #instance_method
-    self.name = n # calling setter method on lines 11-13
+    self.name = n # calling setter method on lines 11-13, same as calling sparky.name=
     self.height = h
     self.weight = w
   end
@@ -18,8 +18,10 @@ class GoodDog
   end
 
   def what_is_self
-    self
+    self #returning the calling object, in this case fido.
   end
+
+  puts self
 end
 
 fido = GoodDog.new("Fido", "6ft", "155lbs")
