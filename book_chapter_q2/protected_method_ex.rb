a5052aba9f39414d3741f888cbb6b63a6ac33d48
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-24 16:01:09 -0700

    protected methods act like public methods inside the class, but like private methods outside the class

--- book_chapter_q2/protected_method_ex.rb
+++ book_chapter_q2/protected_method_ex.rb
@@ -1,6 +1,6 @@
 class Animal
   def a_public_method
-    "Will this work? " + self.a_protected_method
+    "Will this work? " + self.a_protected_method #self.a_protected_method all good, because you can access protected method from inside the class.
   end
 
   protected
@@ -13,3 +13,5 @@ end
 fido = Animal.new
 
 p fido.a_public_method
+
+p fido.a_protected_method # returns error; you can't access protected method from outside the class.
