3a0bc7de3a54ba0ea1e72673f547a57e65a52871
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-23 20:54:14 -0700

    constructor

---
+++ book_chapter_q2/good_dog_v4.rb
@@ -0,0 +1,7 @@
+class GoodDog
+  def initialize #this method referred to as Constructor, because gets triggered whenever new object created.
+    puts "This object was initialized!"
+  end
+end
+
+sparky = GoodDog.new #new is a class method
