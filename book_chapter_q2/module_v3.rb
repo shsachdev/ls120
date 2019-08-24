f1a24c6b9723952ab2812c11261fc59dbb9b9439
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-24 15:23:01 -0700

    exploring new usecases for methods

---
+++ book_chapter_q2/module_v3.rb
@@ -0,0 +1,25 @@
+module Mammal
+  class Dog
+    def speak(sound)
+      p "#{sound}"
+    end
+  end
+
+  class Cat
+    def say_name(name)
+      p "#{name}"
+    end
+  end
+
+  def self.some_out_of_place_method(num)
+    num ** 2
+  end
+end
+
+# buddy = Mammal::Dog.new
+# kitty = Mammal::Cat.new
+# buddy.speak("Arf!")  # => "Arf!"
+# kitty.say_name("kitty") # => "kitty"
+
+
+value = Mammal.some_out_of_place_method(4)
