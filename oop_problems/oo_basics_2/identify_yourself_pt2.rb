1e4a552a59c81dafa0a9da530c4ec14445fd6f25
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-25 15:48:15 -0700

    added setter and getter

--- oop_problems/oo_basics_2/identify_yourself_pt2.rb
+++ oop_problems/oo_basics_2/identify_yourself_pt2.rb
@@ -5,11 +5,11 @@ class Cat
     @name = name
   end
 
-  def to_s # we can override to_s for any class to produce any useful output we need. 
+  def to_s # we can override to_s for any class to produce any useful output we need.
     "I'm #{name}"
   end
 end
 
 kitty = Cat.new("Sophie")
 
-puts kitty
+puts kitty # puts always invokes the to_s method when converting some value to an approximate string representation, so if we can override it, we can get our desired result.
