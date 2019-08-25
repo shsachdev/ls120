8ff9681ead083a68571a7df2e37c30d81150a0c2
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-25 14:18:48 -0700

    created parse_full_name as by DRY principles

--- lesson_2/ex2.rb
+++ lesson_2/ex2.rb
@@ -1,13 +1,24 @@
 class Person
   attr_accessor :first_name, :last_name
+
   def initialize(full_name)
-    parts = full_name.split
-    @first_name = parts.first
-    @last_name = parts.size > 1 ? parts.last : ""
+    parse_full_name(full_name)
   end
-  
-  def name
+
+  def name # getter method
     "#{first_name} #{last_name}".strip
+  end
+
+  def name=(n)
+    parse_full_name(n)
+  end
+
+  private
+
+  def parse_full_name(full_name) # setter method
+    parts = full_name.split
+    self.first_name = parts.first
+    self.last_name = parts.size > 1 ? parts.last : ""
   end
 end
 
@@ -17,3 +28,8 @@ p bob.first_name            # => 'Robert'
 p bob.last_name             # => ''
 bob.last_name = 'Smith'
 p bob.name                  # => 'Robert Smith'
+
+
+bob.name = "John Adams"
+p bob.first_name            # => 'John'
+p bob.last_name             # => 'Adams'
