53965a83efcac4c991f6f4f6cfc920cd775f555b
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-25 15:12:40 -0700

    wrote a class method

--- oop_problems/oo_basics_2/generic_greeting_pt1.rb
+++ oop_problems/oo_basics_2/generic_greeting_pt1.rb
@@ -1,7 +1,9 @@
 class Cat
-  def self.generic_greeting
+  def self.generic_greeting # this could also be defined as Cat.generic_greeting
     puts "Hello! I'm a cat!"
   end
 end
 
 Cat.generic_greeting
+
+kitty = Cat.new
