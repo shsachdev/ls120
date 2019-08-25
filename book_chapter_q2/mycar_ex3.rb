a429ea05e5316bb2e40c7f91e53d8850dc1c2c82
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-24 21:11:45 -0700

    q2 correct

--- book_chapter_q2/mycar_ex3.rb
+++ book_chapter_q2/mycar_ex3.rb
@@ -2,6 +2,8 @@ class Vehicle
   attr_accessor :color, :model, :current_speed
   attr_reader :year
 
+  @@number_of_vehicles = 0
+
   def self.gas_mileage(gallon, miles)
     puts "#{miles / gallons} miles per gallon of gas."
   end
@@ -11,6 +13,7 @@ class Vehicle
     @color = c
     @model = m
     @current_speed = 0
+    @@number_of_vehicles += 1
   end
 
   def speed_up(number)
@@ -31,6 +34,10 @@ class Vehicle
 
   def to_s
     "My year is #{year}, I am #{color}, and I am a #{model}"
+  end
+
+  def self.number_of_vehicles
+    puts @@number_of_vehicles
   end
 end
 
