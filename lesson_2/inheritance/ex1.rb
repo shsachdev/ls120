fb064f11c2b4db04aa2de0a5efc4995c1d4263c6
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-25 16:40:11 -0700

    added class hiearchy

---
+++ lesson_2/inheritance/ex1.rb
@@ -0,0 +1,46 @@
+class Pet
+  def run
+    "running!"
+  end
+
+  def jump
+    "jumping!"
+  end
+end
+
+class Dog < Pet
+  def speak
+    "bark!"
+  end
+
+  def swim
+    "swimming!"
+  end
+
+  def fetch
+    "fetching!"
+  end
+end
+
+class Cat < Pet
+  def speak
+    "meow!"
+  end
+end
+
+class Bulldog < Dog
+  def swim
+    "can't swim!"
+  end
+end
+
+teddy = Dog.new
+
+puts teddy.speak
+puts teddy.swim
+
+karl = Bulldog.new
+
+puts karl.speak
+
+puts karl.swim
