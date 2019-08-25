0a02ce3cf780556d3ecfa13a8d6fa8fff7648b51
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-24 21:39:55 -0700

    wefefr

--- lesson_2/ex1.rb
+++ lesson_2/ex1.rb
@@ -1,14 +1,1 @@
-class Person
-  attr_accessor :name
 
-  def initialize(name)
-    @name = name
-  end
-end
-
-
-
-bob = Person.new('bob')
-bob.name                  # => 'bob'
-bob.name = 'Robert'
-bob.name                  # => 'Robert'
