ccdd8352e87d6d528feb0229f4a6605a33f8e24f
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-25 14:48:41 -0700

    added getter method

--- oop_problems/oo_basics_1/create_the_class.rb
+++ oop_problems/oo_basics_1/create_the_class.rb
@@ -1,10 +1,12 @@
 class Cat
+  attr_reader :name
+
   def initialize(name)
     @name = name
   end
 
   def greet
-    puts "Hello! My name is #{@name}!"
+    puts "Hello! My name is #{name}!"
   end
 end
 
