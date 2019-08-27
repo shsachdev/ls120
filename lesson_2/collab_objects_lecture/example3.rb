204bf5e08c669c0cc6b907864b67b1ddb12b6db9
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-26 23:08:39 -0700

    collaborator objects

---
+++ lesson_2/collab_objects_lecture/example3.rb
@@ -0,0 +1,31 @@
+class Person
+  attr_accessor :name, :pet
+
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+end
+
+
+bob = Person.new("Robert")
+
+kitty = Cat.new
+
+bud = Bulldog.new  # assume bulldog class from previous assignment.
+
+# bob.pet = bud # we have set bob's @pet instance variable to bud, which is a Bulldog object.
+
+# this means that when we call bob.pet, we are returning a Bulldog object.
+
+# because bob.pet returns a Bulldog object, we can chain any Bulldog methods at the end as well.
+
+bob.pets << kitty
+
+bob.pets << bud
+
+bob.pets
+
+bob.pets.each do |pet|
+  pet.jump
+end
