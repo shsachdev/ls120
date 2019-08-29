941e79548392217475208ea6e3772b5a0aee5c41
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-28 22:57:05 -0700

    started working on oop version of rps

---
+++ rps.rb
@@ -0,0 +1,42 @@
+class Player
+  def initialize
+    # maybe a "name"? what about a "move"?
+  end
+
+  def choose
+
+  end
+end
+
+class Move
+  def initialize
+    # seems like we need something to keep track
+    # of the choice... a move object can be "paper", "rock", or "scissors"
+  end
+end
+
+class Rule
+  def initialize
+  # not sure what the "state" of a rule object should be
+  end
+end
+
+# not sure where compare goes yet
+
+def compare(move1, move2)
+
+end
+
+class RPSGame
+  def initialize
+
+  end
+
+  def play
+    display_welcome_message
+    human_choose_move
+    computer_choose_move
+    display_winner
+    display_goodbye_message
+  end
+end
---
+++ rps_logic.md
@@ -0,0 +1,19 @@
+Rock, Paper, Scissors is a two-player game where each player chooses one of
+three possible moves: rock, paper, or scissors. The chosen moves will then be
+compared to see who wins, according to the following rules:
+
+- rock beats scissors
+- scissors beats paper
+- paper beats rock
+
+If the players chose the same move, then it's a tie.
+
+Nouns: player, move, rule
+Verbs: choose, compare
+
+Player
+  - choose
+Move
+Rule
+
+  - compare
