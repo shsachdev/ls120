32d5d3f490c247e4153b41bb68d84cee1816b91b
Shikhar Sachdev <shsachdev@gmail.com>
2019-08-28 21:57:13 -0700

    calm code

---
+++ oop_problems/easy_2/pet_shelter.rb
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+butterscotch = Pet.new('cat', 'Butterscotch')
+pudding      = Pet.new('cat', 'Pudding')
+darwin       = Pet.new('bearded dragon', 'Darwin')
+kennedy      = Pet.new('dog', 'Kennedy')
+sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+molly        = Pet.new('dog', 'Molly')
+chester      = Pet.new('fish', 'Chester')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+
+shelter = Shelter.new
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+shelter.print_adoptions
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
---
+++ oop_problems/easy_2/reverse_engineering.rb
@@ -0,0 +1,19 @@
+class Transform
+  attr_reader :str
+
+  def self.lowercase(string)
+    string.downcase
+  end
+
+  def initialize(str)
+    @str = str
+  end
+
+  def uppercase
+    str.upcase
+  end
+end
+
+my_data = Transform.new('abc')
+puts my_data.uppercase
+puts Transform.lowercase('XYZ')
