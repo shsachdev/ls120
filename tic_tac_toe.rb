ed15ebd2185e890c982de821849e9afe87622e55
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-07 13:20:23 -0700

    working on building tictactoe game using oop

---
+++ tic_tac_toe.rb
@@ -0,0 +1,49 @@
+class Board
+  def initialize
+    # we need some way to model the 3x3 grid. Maybe "squares"?
+    # what data structure should we use?
+    # - array/hash of Square objects?
+    # - array/hash of strings or integers?
+  end
+end
+
+class Square
+  def initialize
+    # maybe a "status" to keep track of this square's mark?
+  end
+end
+
+class Player
+  def initialize
+    # maybe a "marker" to keep track of this player's symbol (ie, "X" or "O")
+  end
+
+  def mark
+
+  end
+
+  def play
+
+  end
+end
+
+class TTTGame
+  def play
+    display_welcome_message
+    loop do
+      display_board
+      first_player_moves
+      break if someone_won? || board_full?
+
+      second_player_moves
+      break if someone_won? || board_full?
+    end
+    display_result
+    display_goodbye_message
+  end
+end
+
+# we'll kick off the game like this
+
+game = TTTGame.new
+game.play
---
+++ tic_tac_toe_logic.md
@@ -0,0 +1,11 @@
+Tic Tac Toe is a 2-player board game played on a 3x3 grid. Players take turns marking a square.
+The first player to mark 3 squares in a row wins.
+
+Nouns: board, player, square, grid
+Verbs: play, mark
+
+Board
+Square
+Player
+  - mark
+  - play
