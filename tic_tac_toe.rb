f50ab5c966348a641d54145fcb83cf4f7a0da2da
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-10 20:18:10 -0700

    moved display board logic from Game class to Board class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -53,6 +53,23 @@ class Board
   def reset
     (1..9).each {|key| @squares[key] = Square.new}
   end
+
+  def draw
+    puts "You're a #{human.marker}. Computer is a #{computer.marker}."
+    puts ""
+    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)} "
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)} "
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)} "
+    puts "     |     |"
+    puts ""
+  end
 end
 
 class Square
@@ -105,17 +122,7 @@ class TTTGame
   def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
-    puts "     |     |"
-    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)} "
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)} "
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)} "
-    puts "     |     |"
+    board.draw 
     puts ""
   end
 
