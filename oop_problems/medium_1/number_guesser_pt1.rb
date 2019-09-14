33b0b359bd21ccda129b89c30d4269136308e5c7
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-14 11:01:21 -0700

    implementing number guesser

---
+++ oop_problems/medium_1/number_guesser_pt1.rb
@@ -0,0 +1,71 @@
+class GuessingGame
+
+  def initialize
+    @num_of_guesses = 7
+    @range = (1..100).to_a
+    @number = (1..100).to_a.sample
+  end
+
+  def play # this is going to be our main method in which the majority of the program is executed
+
+  end
+end
+
+
+game = GuessingGame.new
+game.play
+
+You have 7 guesses remaining.
+Enter a number between 1 and 100: 104
+Invalid guess. Enter a number between 1 and 100: 50
+Your guess is too low.
+
+You have 6 guesses remaining.
+Enter a number between 1 and 100: 75
+Your guess is too low.
+
+You have 5 guesses remaining.
+Enter a number between 1 and 100: 85
+Your guess is too high.
+
+You have 4 guesses remaining.
+Enter a number between 1 and 100: 0
+Invalid guess. Enter a number between 1 and 100: 80
+
+You have 3 guesses remaining.
+Enter a number between 1 and 100: 81
+That's the number!
+
+You won!
+
+game.play
+
+You have 7 guesses remaining.
+Enter a number between 1 and 100: 50
+Your guess is too high.
+
+You have 6 guesses remaining.
+Enter a number between 1 and 100: 25
+Your guess is too low.
+
+You have 5 guesses remaining.
+Enter a number between 1 and 100: 37
+Your guess is too high.
+
+You have 4 guesses remaining.
+Enter a number between 1 and 100: 31
+Your guess is too low.
+
+You have 3 guesses remaining.
+Enter a number between 1 and 100: 34
+Your guess is too high.
+
+You have 2 guesses remaining.
+Enter a number between 1 and 100: 32
+Your guess is too low.
+
+You have 1 guesses remaining.
+Enter a number between 1 and 100: 32
+Your guess is too low.
+
+You have no more guesses. You lost!
