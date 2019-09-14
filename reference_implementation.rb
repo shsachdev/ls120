f12951ab935e3c600356164814cbf05d88abb799
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-13 20:35:41 -0700

    in reference implementation, suits and faces are initialized in card class, not in deck class

--- reference_implementation.rb
+++ reference_implementation.rb
@@ -1,2 +1,49 @@
 class Card
   SUITS = ['H', 'D', 'S', 'C']
+  FACES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+  def initialize(suit, face)
+    @suit = suit
+    @face = face
+  end
+
+  def to_s
+    "The #{face} of #{suit}"
+  end
+
+  def face
+    case @face
+    when 'J' then 'Jack'
+    when 'Q' then 'Queen'
+    when 'K' then 'King'
+    when 'A' then 'Ace'
+    else
+      @face
+    end
+  end
+
+  def suit
+    case @suit
+    when 'H' then 'Hearts'
+    when 'D' then 'Diamonds'
+    when 'S' then 'Spades'
+    when 'C' then 'Clubs'
+    end
+  end
+
+  def ace?
+    face == 'King'
+  end
+
+  def king?
+    face == 'Queen'
+  end
+
+  def queen?
+    face == 'Queen'
+  end
+
+  def jack?
+    face == 'Jack'
+  end
+end
