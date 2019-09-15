9613d12062f58577d85996df23e90f459de9752e
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-15 16:16:58 -0700

    how do i call min and max on an array object

--- oop_problems/medium_1/highest_low_ranking_cards.rb
+++ oop_problems/medium_1/highest_low_ranking_cards.rb
@@ -5,4 +5,61 @@ class Card
     @rank = rank
     @suit = suit
   end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def min
+    
+  end
+
+  def max
+  end
+
+  private
+
+  def value(crd)
+    if crd.to_i != 0
+      crd
+    elsif crd == "Jack"
+      11
+    elsif crd == "Queen"
+      12
+    elsif crd == "King"
+      13
+    elsif crd == "Ace"
+      14
+    end
+  end
 end
+
+
+cards = [Card.new(2, 'Hearts'),
+         Card.new(10, 'Diamonds'),
+         Card.new('Ace', 'Clubs')]
+puts cards
+puts cards.min == Card.new(2, 'Hearts')
+puts cards.max == Card.new('Ace', 'Clubs')
+
+cards = [Card.new(5, 'Hearts')]
+puts cards.min == Card.new(5, 'Hearts')
+puts cards.max == Card.new(5, 'Hearts')
+
+cards = [Card.new(4, 'Hearts'),
+         Card.new(4, 'Diamonds'),
+         Card.new(10, 'Clubs')]
+puts cards.min.rank == 4
+puts cards.max == Card.new(10, 'Clubs')
+
+cards = [Card.new(7, 'Diamonds'),
+         Card.new('Jack', 'Diamonds'),
+         Card.new('Jack', 'Spades')]
+puts cards.min == Card.new(7, 'Diamonds')
+puts cards.max.rank == 'Jack'
+
+cards = [Card.new(8, 'Diamonds'),
+         Card.new(8, 'Clubs'),
+         Card.new(8, 'Spades')]
+puts cards.min.rank == 8
+puts cards.max.rank == 8
