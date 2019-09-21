3bdd25038f3670b1251c8872f58cdcffe73cbddb
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-20 23:36:30 -0700

    solved the anagram question

--- fun_algo.rb
+++ fun_algo.rb
@@ -1,6 +1,26 @@
-def anagram(word, words)
+
+def is_anagram?(word1, word2)
+  hash1 = {}
+  hash2 = {}
+  word1.each_char do |char|
+    hash1[char] = word1.count(char)
+  end
+  word2.each_char do |char|
+    hash2[char] = word2.count(char)
+  end
+  hash1.sort.to_h == hash2.sort.to_h
 end
 
+def anagram(word, words)
+  arr = []
+  words.each do |wrd|
+    arr << wrd if is_anagram?(wrd, word)
+  end
+  p arr
+end
+
+
+puts anagram('abba', ['aabb', 'abcd', 'bbaa', 'dada']) == ['aabb', 'bbaa']
 
 
 # words = []
@@ -20,6 +40,6 @@ end
 # is_anagram? (implementation)
   # iterate through each char of both strings (using each_char)
   # create a hash of the form: char => count (for each string)
-  # compare hashes; if they are equal, then the words are anagrams of each other, and is_anagram? returns true.  
+  # compare hashes; if they are equal, then the words are anagrams of each other, and is_anagram? returns true.
 # 2. if it is, add it to an array.
 # 3. return this array (empty or not)
