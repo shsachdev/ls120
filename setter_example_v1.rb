b3d8a05ec6982e912ae2a62aa5e2c5b56e0c0e9d
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-20 21:52:23 -0700

    more practice for written assessment

--- setter_example_v1.rb
+++ setter_example_v1.rb
@@ -9,3 +9,7 @@ end
 fido = Dog.new("Fido")
 
 puts fido.name
+
+
+# All objects of the same class have the same behaviors, though they contain
+# different states; here, the differing state is the name.
