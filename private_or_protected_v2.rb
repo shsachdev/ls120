99c05a6144201f2db7f7a16bc4620fd3a7dd2195
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-21 21:32:27 -0700

    from inside the class, protected methods are accessible just like public methods

---
+++ private_or_protected_v2.rb
@@ -0,0 +1,48 @@
+class Dog
+  attr_reader :name, :appetite, :food_bowl
+
+  def initialize(name)
+    @name = name
+    @appetite = 10
+    @food_bowl = {"bones" => 10}
+  end
+
+  def bark
+    puts "Woof! Woof!"
+  end
+
+  def feed
+    @food_bowl["bones"] -= 3
+    puts "Here is a bone for you"
+    @appetite -= 5
+    refill
+  end
+
+  def has_more_food_than?(other_dog)
+    amount_of_food > other_dog.amount_of_food
+  end
+
+  protected
+
+  def refill
+    arr = []
+    food_bowl.each do |k, v|
+      arr << k if v == 0
+    end
+    arr.each do |value|
+      @food_bowl[value] += 10
+    end
+  end
+
+  def amount_of_food
+    @food_bowl.values.sum
+  end
+end
+
+fido = Dog.new("fido jones")
+
+larry = Dog.new("larry")
+
+fido.feed
+
+puts fido.has_more_food_than?(larry)
