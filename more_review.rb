040feeb9c82f0e57076441716629035555b01d32
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-21 20:34:52 -0700

    more review

---
+++ more_review.rb
@@ -0,0 +1,17 @@
+# def [](idx)
+#   members[idx]
+# end
+
+# def []=(idx, obj)
+#   members[idx] = obj
+# end
+
+# We refer to the initialize method as a constructor, becaause it gets triggered whenever we create a new object.
+
+# Modules are used only for namespacing and grouping common methods together.
+# Namespacing means organizing similar classes under a module.
+
+# Private methods are not accessible outside of the class definition at all, and are only
+# accessible from inside the class when called without self.
+
+# 
