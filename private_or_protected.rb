b1df2e96db3affbe9bc72d4848e96b2b0624da3e
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-21 21:12:06 -0700

    creating a private method from scratch

--- private_or_protected.rb
+++ private_or_protected.rb
@@ -1,9 +1,10 @@
 class Dog
-  attr_reader :name, :appetite
+  attr_reader :name, :appetite, :food_bowl
 
   def initialize(name)
     @name = name
     @appetite = 10
+    @food_bowl = {"bones" => 10}
   end
 
   def bark
@@ -11,7 +12,28 @@ class Dog
   end
 
   def feed
-    @appetite += 5
-    
+    puts "Here is a bone for you"
+    @appetite -= 5
+    refill
+  end
+
+  private
+
+  def refill
+    arr = []
+    food_bowl.each do |k, v|
+      arr << k if v == 0
+    end
+    arr.each do |value|
+      @food_bowl[value] += 10
+    end
   end
 end
+
+fido = Dog.new("fido jones")
+
+puts fido.appetite
+
+puts fido.food_bowl
+
+puts fido.name
