5ae8a2001e3b5c2f2affbe9e85317b35c61f552d
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-23 21:26:40 -0700

    solved persons.rb by creating custom writer methods

---
+++ revision_problems/persons.rb
@@ -0,0 +1,33 @@
+class Person
+  attr_reader :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name.capitalize
+    @last_name = last_name.capitalize
+  end
+
+  def to_s
+    "#{@first_name} #{@last_name}"
+  end
+
+  def first_name=(n)
+    @first_name = n.capitalize
+  end
+
+  def last_name=(n)
+    @last_name = n.capitalize
+  end
+end
+
+person = Person.new('john', 'doe')
+puts person
+
+person.first_name = 'jane'
+person.last_name = 'smith'
+puts person
+
+
+# Expected Output
+
+# John Doe
+# Jane Smith
