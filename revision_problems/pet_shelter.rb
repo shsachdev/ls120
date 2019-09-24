d498b42f57caf6daeb3639cfc2c35db7a75e96a8
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-23 22:23:54 -0700

    pet shelter problem is tricky

---
+++ revision_problems/pet_shelter.rb
@@ -0,0 +1,70 @@
+class Pet
+  def initialize(animal, name)
+    @animal = animal
+    @name = name
+  end
+end
+
+class Owner
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Shelter
+  def initialize
+    @adoptions = {}
+  end
+
+  def adopt(owner, pet)
+    @owner = owner
+    @pet = pet
+  end
+
+end
+
+
+
+
+
+
+
+
+butterscotch = Pet.new('cat', 'Butterscotch')
+pudding      = Pet.new('cat', 'Pudding')
+darwin       = Pet.new('bearded dragon', 'Darwin')
+kennedy      = Pet.new('dog', 'Kennedy')
+sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+molly        = Pet.new('dog', 'Molly')
+chester      = Pet.new('fish', 'Chester')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+
+shelter = Shelter.new
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+shelter.print_adoptions
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+
+# Expected Return values:
+
+# P Hanson has adopted the following pets:
+# a cat named Butterscotch
+# a cat named Pudding
+# a bearded dragon named Darwin
+#
+# B Holmes has adopted the following pets:
+# a dog named Molly
+# a parakeet named Sweetie Pie
+# a dog named Kennedy
+# a fish named Chester
+#
+# P Hanson has 3 adopted pets.
+# B Holmes has 4 adopted pets.
