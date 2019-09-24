84a16c846df2ef1be6478a38de9746bfa3facdfa
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-23 21:22:00 -0700

    fix books problem complete

--- revision_problems/fix_books.rb
+++ revision_problems/fix_books.rb
@@ -1,4 +1,6 @@
 class Book
+  attr_reader :title, :author
+
   def initialize(author, title)
     @author = author
     @title = title
@@ -12,3 +14,9 @@ end
 book = Book.new("Neil Stephenson", "Snow Crash")
 puts %(The author of "#{book.title}" is #{book.author}.)
 puts %(book = #{book}.)
+
+
+# Expected Output
+
+# The author of "Snow Crash" is Neil Stephenson.
+# book = "Snow Crash", by Neil Stephenson.
