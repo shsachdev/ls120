3e3689bc4447ff954ce2b2ec11d2828cee8db133
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-25 00:18:39 -0700

    fixed it

--- revision_problems/circular_queue_again.rb
+++ revision_problems/circular_queue_again.rb
@@ -9,12 +9,57 @@ class CircularQueue
     if @arr.include?(nil)
       @arr[@arr.index(nil)] = num
     else
-      @arr[0] = num
+      @arr << num
+      @arr.shift
     end
   end
 
-  def dequeue(num)
+  def dequeue
     @arr << nil
-    @arr.shift  
+    @arr.shift
   end
+
 end
+
+queue = CircularQueue.new(3)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+
+
+queue.enqueue(6)
+
+
+queue.enqueue(7)
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
+
+queue = CircularQueue.new(4)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 4
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
