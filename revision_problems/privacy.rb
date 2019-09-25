156c409ccbbdac596deba8c7e70cb069c6a488db
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-24 23:09:27 -0700

    private setter and getter methods

---
+++ revision_problems/privacy.rb
@@ -0,0 +1,15 @@
+class Machine
+  attr_writer :switch
+
+  def start
+    self.flip_switch(:on)
+  end
+
+  def stop
+    self.flip_switch(:off)
+  end
+
+  def flip_switch(desired_state)
+    self.switch = desired_state
+  end
+end
