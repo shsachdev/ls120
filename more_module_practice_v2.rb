5b5439d8ca992d23e3d841549efd15d86862adb0
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-25 23:47:28 -0700

    is it possible to override the new method?

--- more_module_practice_v2.rb
+++ more_module_practice_v2.rb
@@ -1,25 +1,44 @@
-module Speak
-  def speak(sound)
-    puts sound
-  end
-end
-
-class GoodDog
-  include Speak
-end
-
-class HumanBeing
-  include Speak
-end
-
-sparky = GoodDog.new
-
-sparky.speak("Arf!")
-
-bob = HumanBeing.new
-
-bob.speak("Hello!")
+# module Speak
+#   def speak(sound)
+#     puts sound
+#   end
+# end
+#
+# class GoodDog
+#   include Speak
+# end
+#
+# class HumanBeing
+#   include Speak
+# end
+#
+# sparky = GoodDog.new
+#
+# sparky.speak("Arf!")
+#
+# bob = HumanBeing.new
+#
+# bob.speak("Hello!")
 
 
 # Both objects, sparky and bob, have access to the speak instance method.
 # This is possible through "mixing in" the module Speak.
+
+
+# experiment
+
+class Clown
+
+  def initialize(age, gender)
+    @age = age
+    @gender = gender
+  end
+
+  def self.new
+    puts "yolo"
+  end
+end
+
+joker = Class.new(45, "m")
+
+puts joker
