560518da9556c68ecd609c7340eb8b86fcbaed5a
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-25 23:24:20 -0700

    trying to get a really solid understanding of modules

--- more_module_practice.rb
+++ more_module_practice.rb
@@ -1,1 +1,27 @@
 # Why do we need modules? Give us some code examples.
+
+# Modules enable us to mix in behaviors into classes.
+
+module Swim
+  def swim
+    "swimming!"
+  end
+end
+
+class Dog
+  include Swim
+  # ... rest of class omitted
+end
+
+class Fish
+  include Swim
+  # ... rest of class omitted
+end
+
+# The result is that all the instance methods defined in the Swim module are now
+# ready for use in the Dog and Fish classes. It's as if we copied and pasted the
+# methods right in. We can also mix in as many modules as we need to.
+
+# It is important to note that mixing in modules does affect the method lookup path.
+
+# 
