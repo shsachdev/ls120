747f14e4ee936e6ab08bc58f7925e8375698f7d3
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-28 13:53:21 -0700

    solved bug by renaming local variable tasks to something else so as to not ccreate variable conflict

--- task_manager_rev2.rb
+++ task_manager_rev2.rb
@@ -32,9 +32,48 @@ class TaskManager
   end
 
   def display_high_priority_tasks
-    tasks = tasks.select do |task|
+    current_tasks = tasks.select do |task|
       task.priority == :high
     end
 
-    display(tasks)
+    display(current_tasks)
   end
+
+  private
+
+  def display(tasks)
+    puts "------"
+    tasks.each do |task|
+      puts task
+    end
+    puts "--------"
+  end
+end
+
+class Task
+  attr_accessor :name, :priority
+
+  def initialize(name, priority=:normal)
+    @name = name
+    @priority = priority
+  end
+
+  def to_s
+    "[" + sprintf("%-6s", priority) + "] #{name}"
+  end
+end
+
+valentinas_tasks = TaskManager.new("Valentina")
+
+valentinas_tasks.add_task("pay bills", :high)
+
+valentinas_tasks.add_task("read OOP book")
+
+valentinas_tasks.add_task("practice ruby")
+
+valentinas_tasks.add_task("run 5k", :low)
+
+valentinas_tasks.complete_task("read OOP book")
+
+valentinas_tasks.display_all_tasks
+valentinas_tasks.display_high_priority_tasks
