2f69f9da6036b0dc594b909ace5ea770a3e012d9
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-28 13:38:20 -0700

    display tasks question

---
+++ task_manager_rev2.rb
@@ -0,0 +1,40 @@
+class TaskManager
+  attr_reader :owner
+  attr_accessor :tasks
+
+  def initialize(owner)
+    @owner = owner
+    @tasks = []
+  end
+
+  def add_task(name, priority=:normal)
+    task = Task.new(name, priority)
+    tasks.push(task)
+  end
+
+  def complete_task(task_name)
+    completed_task = nil
+
+    tasks.each do |task|
+      completed_task = task if task.name == task_name
+    end
+
+    if completed_task
+      tasks.delete(completed_task)
+      puts "Task '#{completed_task.name}' complete! Removed from list."
+    else
+      puts "Task not found."
+    end
+  end
+
+  def display_all_tasks
+    display(tasks)
+  end
+
+  def display_high_priority_tasks
+    tasks = tasks.select do |task|
+      task.priority == :high
+    end
+
+    display(tasks)
+  end
