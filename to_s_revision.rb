66ddbb2a1315825387cbb15ca2fa0735d705f357
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-29 21:45:20 -0700

    understanding the to_s method

---
+++ to_s_revision.rb
@@ -0,0 +1,5 @@
+# the puts method automatically calls to_s on its argument.
+
+# puts sparky == puts sparky.to_s
+
+# the to_s method returns the name of the object's class and an encoding of the object id.
