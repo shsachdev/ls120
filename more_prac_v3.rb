7327b1c0d9cb0fbb4f47a44109454e853f9b3c4f
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-29 22:34:19 -0700

    collaborator objects

---
+++ more_prac_v3.rb
@@ -0,0 +1,39 @@
+class Cat
+  attr_accessor :name, :age
+
+  def initialize(n, a)
+    @name = n
+    @age = n
+  end
+end
+
+class Bulldog
+  attr_accessor :name, :age
+
+  def initialize(n, a)
+    @name = n
+    @age = a
+  end
+end
+
+class Person
+  attr_accessor :name
+  attr_reader :pets
+
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+end
+
+bob = Person.new("Robert")
+
+kitty = Cat.new("kitty", 12)
+
+bud = Bulldog.new("bud", 16)
+
+bob.pets << kitty
+
+bob.pets << bud
+
+bob.pets
