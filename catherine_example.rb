e601b7730ad96bce0533d3ab09a685f2e9762034
Shikhar Sachdev <shsachdev@gmail.com>
2019-09-30 21:00:17 -0700

    still having trouble understanding this concept

--- catherine_example.rb
+++ catherine_example.rb
@@ -1,5 +1,22 @@
 class Animal
+  def fido
+    self.age
+  end
 end
 
-class Dog
+class Dog < Animal
+  attr_writer :age
+
+  def initialize(n, a)
+    @name = n
+    @age = a
+  end
+
+  def age
+    fido
+  end
 end
+
+fido = Dog.new("fido", 12)
+
+puts fido.age  # what do we get outputted on line 19?
